13d9188b6361568439b27fd94dc22d32fad70d50
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2023-11-02 23:08:15 +0700

    feat: setup stitches

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+import { type ReactElement, type ReactNode } from 'react';
+
 import type { AppProps } from 'next/app';
+import type { NextPage } from 'next/types';
+
+import globalStyle from '@/utils/styles/globalStyle';
+
+type NextPageWithLayout = NextPage & {
+  getLayout: (page: ReactElement) => ReactNode;
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const getLayout = (Component as NextPageWithLayout).getLayout || ((page: ReactNode) => page);
+  globalStyle();
+
+  return getLayout(<Component {...pageProps} />);
 }
 export default MyApp;
